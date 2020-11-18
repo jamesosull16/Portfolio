4ef536e4b7975cc67f0e0e5a615c269a530d9d2d
jamesosull16 <jamesosull16@gmail.com>
2020-11-17 18:27:36 -0700

    changed js to dynamically add buttons and appending the social linksdiv

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,21 @@ $(document).ready(function () {
   const personalInfo = $("#personal");
   const name = $("<h1>");
   const job = $("<h3>");
-
-  const aboutBtn = $("#aboutBtn");
-  const projectsBtn = $("#projectsBtn");
-  const resumeBtn = $("#resumeBtn");
-
-  const githubBtn = $("#githubBtn");
-  const linkedinBtn = $("#linkedinBtn");
+  const aboutBtn = $(
+    "<button>",
+    { class: "btn btn-outline-info" },
+    { id: "aboutBtn" }
+  );
+  const projectsBtn = $(
+    "<button>",
+    { class: "btn btn-outline-info" },
+    { id: "projectsBtn" }
+  );
+  const resumeBtn = $(
+    "<button>",
+    { class: "btn btn-outline-info" },
+    { id: "resumeBtn" }
+  );
 
   name.text("James O'Sullivan");
   job.text("Software Developer");
@@ -16,17 +24,36 @@ $(document).ready(function () {
   aboutBtn.text("About Me");
   projectsBtn.text("Projects");
   resumeBtn.text("Resume");
-  githubBtn.text("Github");
-  linkedinBtn.text("LinkedIn");
 
   personalInfo.append(name);
   personalInfo.append(job);
   personalInfo.append(aboutBtn);
   personalInfo.append(projectsBtn);
   personalInfo.append(resumeBtn);
-  personalInfo.append(githubBtn);
-  personalInfo.append(linkedinBtn);
 
+  //social images and links
+  const socialLinks = $("#social");
+
+  const githubBtn = $(
+    "<button>",
+    { class: "btn btn-outline-info" },
+    { id: "githubBtn" }
+  );
+  const linkedinBtn = $(
+    "<button>",
+    { class: "btn btn-outline-info" },
+    { id: "linkedinBtn" }
+  );
+
+  githubBtn.text("Github");
+  linkedinBtn.text("LinkedIn");
+
+  socialLinks.append(githubBtn);
+  socialLinks.append(linkedinBtn);
+
+  personalInfo.append(socialLinks);
+
+  //event listeners
   $("#aboutBtn").on("click", function () {
     location.href = "./assets/about.html";
   });
@@ -36,14 +63,17 @@ $(document).ready(function () {
   });
 
   $("#resumeBtn").on("click", function () {
-    location.href = "./assets/Resume2021.pdf";
+    window.open("./assets/Resume2021.pdf", "_blank");
   });
 
   $("#githubBtn").on("click", function () {
-    location.href = "https://github.com/jamesosull16";
+    window.open("https://github.com/jamesosull16", "_blank");
   });
 
   $("#linkedinBtn").on("click", function () {
-    location.href = "https://www.linkedin.com/in/james-o-sullivan-5081b941/";
+    window.open(
+      "https://www.linkedin.com/in/james-o-sullivan-5081b941/",
+      "_blank"
+    );
   });
 });
